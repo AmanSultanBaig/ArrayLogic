53473f1c8f1ac16c0905705480f308eaf2b9990e
Aman Sultan Baig <amansultanbaig@gmail.com>
2020-01-06 15:48:55 +0500

    file done with logic

--- array.js
+++ array.js
@@ -1,1 +1,14 @@
-let arr = [8,4,6,8,9,7];
+// expected output
+
+/* 96 92 97 92
+   66 62 67 62
+   26 22 27 22
+   76 72 77 72
+   26 22 27 22 */
+
+let arr = [9,6,2,7,2];
+for(let i = 0; i < arr.length; i++) {
+  for(let j = 1; j < arr.length; j++) {
+    console.log(arr[i]+''+arr[j])
+  }
+}
